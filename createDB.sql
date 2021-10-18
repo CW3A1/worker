a01c5b78f10dcaf4931f99a50ddefd085b3599be
Mohammed Andiche <mohammed.andiche@student.kuleuven.be>
2021-10-18 16:19:52 +0200

    mv sql file

--- createDB.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE "tasks" (task_id TEXT PRIMARY KEY, status INTEGER NOT NULL DEFAULT '0', unix_time INTEGER);
-CREATE TABLE "task_scheduler" (pc TEXT PRIMARY KEY, status INTEGER NOT NULL DEFAULT '0');
-INSERT INTO task_scheduler (pc) VALUES ('eeklo'), ('beveren'), ('damme'), ('halle'), ('brugge');
