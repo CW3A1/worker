2acf48d7d16b5742718623cfd3aaace7e1a7e26d
Mohammed Andiche <mohammed.andiche@student.kuleuven.be>
2021-11-05 16:42:23 +0100

    fix add task not returning response

--- routers/tasks.py
+++ routers/tasks.py
@@ -25,9 +25,9 @@ class TaskOutput(BaseModel):
 async def add_task(task_data: TaskInput, background: BackgroundTasks, identifier: str = Depends(auth.header_to_identifier)):
     task_id = str(uuid.uuid4())[:8]
     r = [task_data.p1[0], task_data.p2[0], task_data.p3[0], task_data.p4[0], task_data.p1[1], task_data.p2[1], task_data.p3[1], task_data.p4[1]]
-    resp = database.add_task(task_id, r, identifier)
+    database.add_task(task_id, r, identifier)
     background.add_task(openfoam.next_openfoam_thread)
-    return resp
+    return database.status_task(task_id)
 
 @router.get("/status/{task_id}", response_model=TaskOutput, tags=["tasks"])
 async def view_task_status(task_id: str, identifier: str = Depends(auth.header_to_identifier)):
