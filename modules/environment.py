a9d1c5b61817a5b010277cab32f292123b7c2444
Mohammed Andiche <mohammed.andiche@student.kuleuven.be>
2021-11-08 17:19:45 +0100

    change to worker files

--- modules/environment.py
+++
@@ -1,8 +0,0 @@
-from os import getenv
-
-DB_FILE = getenv("db_file")
-DB_TABLE_TASKS = getenv("db_table_tasks")
-DB_TABLE_SCHEDULER = getenv("db_table_scheduler")
-DB_TABLE_USERS = getenv("db_table_users")
-AUTH_SECRET = getenv("auth_secret")
-WS_URL = getenv("ws_url")
