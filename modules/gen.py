d30697a03250a09f376a50207858c0cf976327c0
Mohammed Andiche <mohammed.andiche@student.kuleuven.be>
2021-11-11 09:35:00 +0100

    simplify and improve eval string

--- modules/gen.py
+++ modules/gen.py
@@ -1,29 +1,21 @@
 import math
 
-import numpy as np
+import numpy
 import scipy.special as special
 from numpy import inf as Inf
 from sympy import Poly, init_printing
 from sympy.abc import *
-from sympy.parsing.sympy_parser import parse_expr
+from sympy.parsing.sympy_parser import (convert_xor,
+                                        implicit_multiplication_application,
+                                        parse_expr, standard_transformations)
 from sympy.utilities.lambdify import lambdify
 
 init_printing(use_unicode=True)
 
-def oneDEvalString(f: str):
-    eval_string = parse_expr(f)
-    f = lambdify((x), eval_string)
+def evalString(f: str):
+    parse_string = parse_expr(f, transformations=standard_transformations+(convert_xor, implicit_multiplication_application,))
+    f = lambdify(tuple(parse_string.free_symbols), parse_string, "numpy")
     return f
 
-def twoDEvalString(f: str):
-    eval_string = parse_expr(f)
-    f = lambdify((x,y), eval_string)
-    return f
-
-def threeDEvalString(f: str):
-    eval_string = parse_expr(f)
-    f = lambdify((x,y,z), eval_string)
-    return f
-
-def oneDPolyToStr(f: np.poly1d):
+def oneDPolyToStr(f: numpy.poly1d):
     return str(Poly([round(num, 5) for num in f.coef], x).as_expr())
