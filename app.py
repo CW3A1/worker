de0c5e59fe879b55146773dfa6050f19994dbe45
Mohammed Andiche <mohammed.andiche@student.kuleuven.be>
2021-11-22 18:48:39 +0100

    now its blocking fuck me

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from asyncio import ensure_future
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
+from trio import run
 
 from modules import classes, heat_equation, num_math
 
@@ -18,30 +17,30 @@ app.add_middleware(GZipMiddleware)
 
 @app.post("/num_math/differentiation")
 async def new_task(task_input: classes.DiffOptions):
-    ensure_future(num_math.numDiff(task_input.task_id, task_input.f, task_input.a, task_input.order))
+    run(num_math.numDiff, *(task_input.task_id, task_input.f, task_input.a, task_input.order))
     return {"response": "OK"}
 
 @app.post("/num_math/integration")
 async def new_task(task_input: classes.IntOptions):
-    ensure_future(num_math.numInt(task_input.task_id, task_input.f, task_input.a, task_input.b))
+    run(num_math.numInt, *(task_input.task_id, task_input.f, task_input.a, task_input.b))
     return {"response": "OK"}
 
 @app.post("/num_math/optimization")
 async def new_task(task_input: classes.OptimOptions):
-    ensure_future(num_math.twoDNumOpt(task_input.task_id, task_input.f, task_input.xl, task_input.xu, task_input.yl, task_input.yu))
+    run(num_math.twoDNumOpt, *(task_input.task_id, task_input.f, task_input.xl, task_input.xu, task_input.yl, task_input.yu))
     return {"response": "OK"}
 
 @app.post("/num_math/lagrange_interpolation")
 async def new_task(task_input: classes.LagrangeOptions):
-    ensure_future(num_math.lagrangePoly(task_input.task_id, task_input.a, task_input.b))
+    run(num_math.lagrangePoly, *(task_input.task_id, task_input.a, task_input.b))
     return {"response": "OK"}
 
 @app.post("/num_math/taylor_approximation")
 async def new_task(task_input: classes.TaylorOptions):
-    ensure_future(num_math.approximateTaylorPoly(task_input.task_id, task_input.f, task_input.x0, task_input.order))
+    run(num_math.approximateTaylorPoly, *(task_input.task_id, task_input.f, task_input.x0, task_input.order))
     return {"response": "OK"}
 
 @app.post("/num_math/heat_equation")
 async def new_task(task_input: classes.HeatOptions):
-    ensure_future(heat_equation.calcAnimUp(task_input.task_id, task_input))
+    run(heat_equation.calcAnimUp, *(task_input.task_id, task_input))
     return {"response": "OK"}
