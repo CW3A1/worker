d6379d96e94f9c171aa4275cea6e7f725201f0ad
Mohammed Andiche <mohammed.andiche@student.kuleuven.be>
2021-12-07 18:40:15 +0100

    add plot for diff and update taylor plot

--- modules/num_math.py
+++ modules/num_math.py
@@ -2,11 +2,13 @@ from time import time
 from typing import List
 
 import matplotlib.pyplot as plt
-from numpy import array, linspace
+from numpy import array, linspace, vectorize
 from scipy.integrate import quad
 from scipy.interpolate import approximate_taylor_polynomial, lagrange
 from scipy.misc import derivative
 from scipy.optimize import dual_annealing
+from scipy.special import factorial
+from sympy import Float, preorder_traversal, simplify
 from sympy.abc import *
 
 from modules.gen import *
@@ -16,10 +18,16 @@ def numDiff(task_id: str, f: str, a: float, o: int = 1):
     try:
         f = evalString(f)
         result = derivative(f, a, n=o, order=max(o+1+(o)%2, 7))
-        result = {"result": result}
         add_log(f"Calculated derivative for task {task_id}")
+        try:
+            link = plotDiff(f, a, o)
+            add_log(f"Generated plot for for task {task_id}")
+        except:
+            link = "error"
+            add_log(f"Failed to generate plot for task {task_id}")
+        result = {"result": result, "link": link}
     except:
-        result = {"result": "error"}
+        result = {"result": "error", "link": "error"}
         add_log(f"Failed to calculate derivative for task {task_id}")
     finally:
         postToDB(task_id, result)
@@ -96,6 +104,46 @@ def approximateTaylorPoly(task_id: str, f: str, x0: float, degree: int):
     finally:
         postToDB(task_id, result)
 
+def plotDiff(f: str, a: float, o: int):
+    fun = evalString(f)
+    dx = linspace(start=0 if a>0 else (2 * a if a < 0 else -10), stop=2*a if a>0 else (0 if a < 0 else 10), num=100)
+    if o == 1:
+        differentiaal = derivative(fun, a, n=o, order=max(o+1+(o)%2, 7))
+        f0 = fun(a)
+        raaklijn = lambda x: (f0 - differentiaal * a) + differentiaal * x
+        raaklijn_vector = vectorize(raaklijn)
+        raaklijn = raaklijn_vector(dx)
+        plt.plot(dx, raaklijn, '#1f2937')
+        legend_approximation = "Tangent line in given point"
+    else:
+        afgeleide = []
+        for i in dx[1:len(dx) - 1]:
+            afgeleide.append(derivative(f, i, n=o, order=max(o+1+(o)%2, 7)))
+        plt.plot(dx[1: len(dx) - 1], afgeleide, '#1f2937')
+        legend_approximation = "Derivative"
+        a_vector = [a] * 5
+        f_accent_a = afgeleide[len(afgeleide) // 2]
+        if f_accent_a > 0:
+            vert_points = numpy.linspace(0, f_accent_a, num=5)
+        else:
+            vert_points = numpy.linspace(f_accent_a, 0, num=5)
+        plt.plot(a_vector, vert_points, color='r')
+    f_vector = vectorize(f)
+    f = f_vector(dx)
+    plt.plot(dx, f, '#5599ff')
+    plt.xlabel("X-axis")
+    plt.ylabel('Y-axis')
+    plt.title('Derivative')
+    if o == 1:
+        plt.legend([legend_approximation, 'Original function'])
+    else:
+        plt.legend([legend_approximation, 'Derivative in chosen point', 'Original function'])
+        plt.scatter(a, f_accent_a, color='r', linewidths=0.01)
+    f = f"/tmp/{time()}.png"
+    plt.savefig(f)
+    link = uploadToUguu(f)
+    return link
+
 def plotIntegral(f, a: float, b: float):
     x_begin = a - (b-a)*0.2
     x_eind = b + (b-a)*0.2
@@ -129,18 +177,30 @@ def plotLagrange(f: str, a: List[float], b: List[float]):
     link = uploadToUguu(f)
     return link
 
-def plotTaylor(f1: str, f2: str, x0: float):
-    fun_1 = evalString(f1)
-    fun_2 = evalString(f2)
-    dx = linspace(start = x0-3, stop = x0+3, num = 51)
-    plot = plt.figure()
-    plt.plot(dx, fun_1(dx))
-    plt.plot(dx, fun_2(dx))
-    plt.xlabel('X-axis')
+def plotTaylor(f: str, a: float, o: int):
+    fun = evalString(f)
+    taylor = fun(a)
+    for i in range(1, o+1):
+        taylor = taylor + derivative(fun, a, n=i, order=max(o+1+o%2, 7))*(x-a)**i/factorial(i)
+    taylor = simplify(taylor)
+    taylor_round = taylor
+    for r in preorder_traversal(taylor):
+        if isinstance(r, Float):
+            taylor_round = taylor_round.subs(r, round(r, 1))
+    taylor_lambda = lambdify(x, taylor_round)
+    dx = linspace(start=0 if a>0 else (2*a if a<0 else -10), stop=2*a if a>0 else (0 if a<0 else 10), num = 100)
+    f_vector = vectorize(f)
+    taylor_vector = vectorize(taylor_lambda)
+    taylor = taylor_vector(dx)
+    fv = f_vector(dx)
+    plt.plot(dx, taylor, color = "#1f2937")
+    plt.plot(dx, fv, color = "#5599ff")
+    plt.scatter(a,f(a),color='r')
+    plt.xlabel("X-axis")
     plt.ylabel('Y-axis')
-    plt.legend(['Original function',
-                'Approximated function'])
-    plt.title(f'Taylor approximation of {f1} around {x0}')
+    plt.title('Taylor approximation')
+    plt.legend(['Taylor approximation','Original function'])
+    plt.show()
     f = f"/tmp/{time()}.png"
     plt.savefig(f)
     link = uploadToUguu(f)
